87de7991d437ad7c7bc0cdccbafc00c6e8ef3a92
Fiona Condon <frcondon@gmail.com>
2016-04-20 22:42:15 -0400

    Adding sign in button that does nothing

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def root():
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
